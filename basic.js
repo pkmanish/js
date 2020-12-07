ee99c28d13e0887f721db59b36f1deffd36aa012
pkmanish <manishkumarrr5@gmail.com>
2020-12-08 00:51:07 +0530

    initial commit

---
+++ basic.js
@@ -0,0 +1,375 @@
+//console.log('manish');
+//you can write comments like this .
+
+//Variables
+//it is contaner where we can store our data
+/*
+let firstName = 'manish';
+let lastname = 'pathak';
+console.log(firstName);
+console.log(lastname);
+console.log(firstName, lastname);
+*/
+
+//we can also change the value of variable
+//variable will also vary 
+//how to change variable name 
+
+//let firstName = 'manish';
+//let lastname = 'pathak';
+//firstName = 'bitu';
+//console.log(firstName, lastname);
+
+//let fullName = 'manish pathak';
+//console.log(fullName);
+
+//how to put value in variable 
+
+//let yourName;
+//yourName = 'Ashish pathak'
+//console.log(yourName);
+
+//constants 
+//constants is also contaner like variable but in constants we can naver change the value of constants 
+//it is constant
+//const MyfullName = 'Anjali pathak';
+//console.log(MyfullName);
+
+
+
+//variable naming rules .
+//1 No spaces b/t the name
+//2 Must begin with _,$ or a letter
+//3 Can contain letter , numbers , underscore and doller sign
+//4 Case sensitive
+
+
+// Data type 
+// 1 - String 
+// Normal text 
+//let firstName = 'Kanchan';
+//let lastName = ' devi';
+
+
+// 2 number 
+// we can store any integer or desmal value
+// Ex 
+// let num1 = 3445;
+// let num2 = 0;
+// let num0 = 45.65
+
+// 3 Boolean
+// - in bollean we can only use two statement True or False
+// Ex
+// let MyBool = true;
+// let MyBool = false;
+
+
+// 4 undefine 
+// Where we creat a variable but not putting the value that is called undefine .
+// Ex
+//let fullName ;
+//console.log(fullName);
+// O/P- undefined S
+
+
+// String 
+//Adding of two String 
+//let firstName =' manish';
+//let lastName =' pathak';
+//let fullName = firstName+" "+lastName;
+//console.log(fullName);
+
+//let fullName =`${firstName} ${lastName}`;
+//console.log(fullName);
+//console.log(fullName.toUpperCase());
+//console.log(fullName.toLowerCase());
+//console.log(firstName.length);
+
+
+//How to add special symbole in string 
+// Throu \ back slace 
+
+// Example 
+// let message =  'It\'s ok';
+
+
+
+//Arithmetic operators
+// 1 Addition +
+// 2 Substraction - 
+// 3 Multiplication *
+// 4 Divide /
+// 5 Remainder %
+// 6 Increment ++
+// 7 Decrement --
+// 8 Exponentiation **
+
+
+/*
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2;
+var sub = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+var rem = num1 % num2;
+console.log(num1, num2)
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(rem);
+
+*/
+
+// let fruits = ['Apple', 'Mango', 'Banana'];
+// let condingLanguages = ['Javascript', 'Python', 'Go'];
+ 
+// console.log(fruits);
+// console.log(condingLanguages);
+// console.log(fruits[2]);
+// console.log(condingLanguages[0]);
+
+
+//fruits.push('orange');
+// console.log(fruits);
+//fruits.unshift('Watermelon');
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+// fruits.pop();
+
+
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+ 
+// fruits.shift();
+// console.log(fruits);
+ 
+// console.log(fruits.length);
+
+
+// var friends = new Array('manish', 'Ashish');
+// console.log(friends); 
+
+//  let person = {
+//      fristName: 'manish',
+//      lastName: 'Pathak',
+//      hobbies: ['Writing', 'coding'],
+//      address:{
+//          state: 'Bihar',
+//         country: 'India',
+//          }
+//  }
+// console.log(person.fristName, person.lastName);
+
+// person.hobbies.push('cricket');
+
+// //console.log(person.hobbies.length);
+// person.fristName = 'manish'
+// console.log(person.fristName)
+
+
+// person.age = 19;
+// console.log(person.age)
+//console.age(person.address.state)
+
+// person.twitter = 'manishkumarrr5@gmail.com'
+// console.age(person.twitter)
+
+
+// let persons = [
+//     {
+//         name: 'Manish pathak',
+//         age: 22,
+//         isCoder: true,
+//     },
+
+//     {
+//         name: 'Ashish pathak',
+//         age: 25,
+//         isCoder: false,
+//     },
+
+//     {
+//         name: 'Anjali pathak',
+//         age: 27,
+//         isCoder: false,
+//     },
+// ];
+
+// console.log(persons[0]);
+// persons[0].name = 'Manish';
+// console.log(persons[0]);
+
+
+
+
+
+// let age = 10;
+
+// if (age === 10 ) {
+//     console.log('Age is 10');
+// } else {
+//     console.log('Age is not equle  to 10 ');
+
+// }
+
+// comparison poerators 
+   // comparison operators are used to compare values 
+// let age =12;
+// if (age > 18) {
+//     console.log('Age is greater than 18');
+// } else {
+//     console.log('Age is less than 18');
+// }
+
+
+
+/*
+
+let age =  16;
+if (age <18){
+    console.log('Age is less than 18');
+}else{
+    console.log('Age is greater than 18 ')
+    
+}
+
+// >=
+
+let age =  16;
+if (age >=18){
+    console.log('Age is greater than or equal to  18');
+}else{
+    console.log('Age is less  than 18 ')
+    
+}
+
+
+/*<=
+
+let age =  18;
+if (age <=18){
+    console.log('Age is less than or equal to  18');
+}else{
+    console.log('Age is greater than 18 ')
+    
+}
+
+
+==
+
+let age =  16;
+if (age == 18){
+    console.log('Age is equak to  18');
+}else{
+    console.log('Age is not equal to  18 ')
+    
+}
+!=
+
+let age =  19;
+if (age != 18){
+    console.log('Age is not equal to  18');
+}else{
+    console.log('Age is equal to  18 ')
+    
+}
+===
+
+let age =  16;
+if (age === 18){
+    console.log('Age is equal to  18');
+}else{
+    console.log('Age is not equal to  18')
+    
+}
+!==
+
+let age =  18;
+if (age !==18){
+    console.log('Age is not equal to  18');
+}else{
+    console.log('Age is equal to  18')
+    
+} */
+
+// let age = 18;
+// if (age === 2){
+//     console.log('age is 18')
+// }else if (age >18){
+//     console.log('Age is greater than 18')
+
+// }else {
+//     console.log('Age is less than 18')
+// }
+
+
+// let fruit = 'Apple'
+// switch (fruit){
+//     case 'Apple':
+//         console.log('your fav fruit is apple')
+//         console.log('hello')
+//         break;
+
+//         case 'Mango':
+//             console.log('your fav fruit is Mango')
+
+
+//          break;
+//          case 'banana':
+//              console.log('banana')
+//              break;
+             
+//          case 'Watermelon':
+//                  console.log('Watermelon')
+//                  break;
+//                  default:
+//                      console.log('Pleas enter a differemt fruit name ');
+//             }
+
+
+// let no = 3;
+// switch(no){
+//     case 1 :
+//         console.log('one');
+//         break;
+
+//         case 2 :
+//             console.log('two');
+//             break;
+
+//         case 3 :
+//             console.log('three');
+//             break;
+
+//             default:
+//            console.log('Enter a different number');
+           
+// }
+
+
+// 
+
+// for (let i = 0; i < 10; i++)  {
+//     console.log(i, 'Hello world');
+// }
+
+
+
+// for (let i = 8; i > 0; i--)  {
+//     console.log(i, 'Hello world');
+// }
+
+
+// let fruits = ['Apple', 'Mango', 'Banana'];
+ 
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+
